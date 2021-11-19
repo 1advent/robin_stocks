9ccd75bc83eb96fe832b74bf11c264a8039e1a83
Joshua Welch <oneadvent@joshuawelch.me>
2021-11-18 18:07:09 -0800

    fixed double round and put it on a loop

--- get_stocks.py
+++ get_stocks.py
@@ -1,43 +1,48 @@
 import pyotp
 import robin_stocks.robinhood as r
 
+import time
+
+
 from decouple import config
 
 totp = pyotp.TOTP(config('robin_mfa')).now()
 login = r.login(config('robin_username'),config('robin_password'),mfa_code=totp)
 stocks = r.build_holdings()
 
-result_list = []
+for i in range(10):
 
-for stock,value in stocks.items():
-    list = {
-        "code":stock,
-        'invested':float(value["average_buy_price"]), 
-        'quantity':float(value["quantity"]), 
-        'current_price': float(value["price"]),
-        'price_point':float(value["average_buy_price"]) * float(value["quantity"])
-        }
-    result_list.append(list)
+    result_list = []
 
-
-cryptos = r.get_crypto_positions()
-
-for d_ in cryptos:
-        quantity = float(d_["quantity"])
-        code = d_["currency"]["code"]
-        cost = float(d_["cost_bases"][0]["direct_cost_basis"])
-        price_point = cost/quantity
-        _ = r.get_crypto_quote(code)
-        current_price = round(float(_["ask_price"]),4)
+    for stock,value in stocks.items():
         list = {
-            'code':code, 
-            'invested': cost, 
-            'quanitity':quantity, 
-            'price_point':price_point,
-            'current_price': round(current_price, 2)
+            "code":stock,
+            'invested':float(value["average_buy_price"]), 
+            'quantity':float(value["quantity"]), 
+            'current_price': float(value["price"]),
+            'price_point':float(value["average_buy_price"]) * float(value["quantity"])
             }
         result_list.append(list)
-"""
-for vs in result_list:
-    print(vs["code"] + " is " + str(vs["current_price"]))
-"""
+
+    cryptos = r.get_crypto_positions()
+
+    for d_ in cryptos:
+            quantity = float(d_["quantity"])
+            code = d_["currency"]["code"]
+            cost = float(d_["cost_bases"][0]["direct_cost_basis"])
+            price_point = cost/quantity
+            _ = r.get_crypto_quote(code)
+            current_price = round(float(_["ask_price"]),4)
+            list = {
+                'code':code, 
+                'invested': cost, 
+                'quanitity':quantity, 
+                'price_point':price_point,
+                'current_price': current_price,
+                }
+            result_list.append(list)
+
+    for vs in result_list:
+        print(vs["code"] + " is " + str(vs["current_price"]))
+    print("************")
+    time.sleep(10)
